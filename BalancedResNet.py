3b1238b489b5bafd54370bf31831e1bd58ed9dd4
AlisaH612 <47835653+AlisaH612@users.noreply.github.com>
2022-05-19 20:14:53 -0700

    “update”

---
+++ BalancedResNet.py
@@ -0,0 +1,182 @@
+import os
+import random
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import PIL
+import PIL.Image
+import matplotlib.pyplot as plt
+from sklearn.utils import class_weight
+from sklearn.metrics import confusion_matrix, cohen_kappa_score
+from tensorflow import keras
+from keras import backend as K
+from keras.models import Model
+from keras import optimizers, applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, Callback
+from keras.layers import Dense, Dropout, GlobalAveragePooling2D, Input
+from tensorflow.keras.applications.resnet50 import ResNet50
+
+# Set seeds to make the experiment more reproducible.
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+def seed_everything(seed=0):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(0)
+seed_everything()
+
+%matplotlib inline
+sns.set(style="whitegrid")
+warnings.filterwarnings("ignore")
+
+# Model parameters
+BATCH_SIZE = 8
+EPOCHS = 30
+WARMUP_EPOCHS = 2
+LEARNING_RATE = 1e-4
+WARMUP_LEARNING_RATE = 1e-3
+HEIGHT = 224
+WIDTH = 224
+CANAL = 3
+N_CLASSES = 5
+ES_PATIENCE = 5
+RLROP_PATIENCE = 3
+DECAY_DROP = 0.5
+
+def kappa(y_true, y_pred, n_classes=5):
+    y_trues = K.cast(K.argmax(y_true), K.floatx())
+    y_preds = K.cast(K.argmax(y_pred), K.floatx())
+    n_samples = K.cast(K.shape(y_true)[0], K.floatx())
+    distance = K.sum(K.abs(y_trues - y_preds))
+    max_distance = n_classes - 1
+    
+    kappa_score = 1 - ((distance**2) / (n_samples * (max_distance**2)))
+
+    return kappa_score
+
+train_dir = '/home/ubuntu/dataset/outputs/train'
+val_dir = '/home/ubuntu/dataset/outputs/val'
+test_dir = '/home/ubuntu/dataset/outputs/test'
+train_datagen=ImageDataGenerator(rescale=1./255, 
+                                 horizontal_flip=True,
+                                 vertical_flip=True)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(HEIGHT, WIDTH),
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    ) 
+
+valid_generator=train_datagen.flow_from_directory(
+    val_dir,
+    target_size=(HEIGHT, WIDTH),
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",    
+    )
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_generator = test_datagen.flow_from_directory(  
+        test_dir,
+        target_size=(HEIGHT, WIDTH),
+        batch_size=1,
+        shuffle=False,
+        class_mode=None)
+
+def create_model(input_shape, n_out):
+    input_tensor = Input(shape=input_shape)
+    base_model = ResNet50(weights='imagenet', 
+                                       include_top=False,
+                                       input_tensor=input_tensor)
+    
+
+    x = GlobalAveragePooling2D()(base_model.output)
+    x = Dropout(0.5)(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    final_output = Dense(n_out, activation='softmax', name='final_output')(x)
+    model = Model(input_tensor, final_output)
+    
+    return model
+
+model = create_model(input_shape=(HEIGHT, WIDTH, CANAL), n_out=N_CLASSES)
+
+for layer in model.layers:
+    layer.trainable = False
+
+for i in range(-5, 0):
+    model.layers[i].trainable = True
+    
+class_weights = {0: 0.27219,
+                1: 2.8756,
+                2: 1.3275,
+                3: 8.047,
+                4: 9.9226}
+
+metric_list = ["accuracy", kappa]
+optimizer = tf.keras.optimizers.Adam(lr=WARMUP_LEARNING_RATE)
+model.compile(optimizer=optimizer, loss="categorical_crossentropy",  metrics=metric_list)
+
+STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
+STEP_SIZE_VALID = valid_generator.n//valid_generator.batch_size
+
+history_warmup = model.fit_generator(generator=train_generator,
+                                     steps_per_epoch=STEP_SIZE_TRAIN,
+                                     validation_data=valid_generator,
+                                     validation_steps=STEP_SIZE_VALID,
+                                     epochs=WARMUP_EPOCHS,
+                                     class_weight=class_weights,
+                                     verbose=1).history
+
+for layer in model.layers:
+    layer.trainable = True
+
+es = EarlyStopping(monitor='val_loss', mode='min', patience=ES_PATIENCE, restore_best_weights=True, verbose=1)
+rlrop = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=RLROP_PATIENCE, factor=DECAY_DROP, min_lr=1e-6, verbose=1)
+
+callback_list = [es, rlrop]
+optimizer = tf.keras.optimizers.Adam(lr=LEARNING_RATE)
+model.compile(optimizer=optimizer, loss="categorical_crossentropy",  metrics=metric_list)
+
+history_finetunning = model.fit_generator(generator=train_generator,
+                                          steps_per_epoch=STEP_SIZE_TRAIN,
+                                          validation_data=valid_generator,
+                                          validation_steps=STEP_SIZE_VALID,
+                                          epochs=EPOCHS,
+                                          callbacks=callback_list,
+                                          class_weight=class_weights,
+                                          verbose=1).history
+
+history = {'loss': history_warmup['loss'] + history_finetunning['loss'], 
+           'val_loss': history_warmup['val_loss'] + history_finetunning['val_loss'], 
+           'accuracy': history_warmup['accuracy'] + history_finetunning['accuracy'], 
+           'val_accuracy': history_warmup['val_accuracy'] + history_finetunning['val_accuracy'], 
+           'kappa': history_warmup['kappa'] + history_finetunning['kappa'], 
+           'val_kappa': history_warmup['val_kappa'] + history_finetunning['val_kappa']}
+
+sns.set_style("whitegrid")
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', figsize=(20, 18))
+
+ax1.plot(history['loss'], label='Train loss')
+ax1.plot(history['val_loss'], label='Validation loss')
+ax1.legend(loc='best')
+ax1.set_title('Loss')
+
+ax2.plot(history['accuracy'], label='Train accuracy')
+ax2.plot(history['val_accuracy'], label='Validation accuracy')
+ax2.legend(loc='best')
+ax2.set_title('Accuracy')
+
+ax3.plot(history['kappa'], label='Train kappa')
+ax3.plot(history['val_kappa'], label='Validation kappa')
+ax3.legend(loc='best')
+ax3.set_title('Kappa')
+
+plt.xlabel('Epochs')
+sns.despine()
+plt.show()
