af2c7e8a87a0e55ec1608f09b3cdcd9785d01df1
shunyao <xsy981212@gmail.com>
2022-05-22 12:10:28 -0700

    update

--- swin_transformer/delete_images.py
+++ swin_transformer/delete_images.py
@@ -1,9 +1,13 @@
 import os
 
 files = os.listdir('/home/ubuntu/dataset/0')
-for file in files[3000:]:
+for file in files[2000:]:
     os.remove('/home/ubuntu/dataset/0/' + file)
     
+files = os.listdir('/home/ubuntu/dataset/1')
+for file in files[2000:]:
+    os.remove('/home/ubuntu/dataset/1/' + file)
+    
 files = os.listdir('/home/ubuntu/dataset/2')
-for file in files[3000:]:
+for file in files[2000:]:
     os.remove('/home/ubuntu/dataset/2/' + file)
