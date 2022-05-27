263e6a35ff711ef64e2cca169a3f15a12f507af3
shunyao <xsy981212@gmail.com>
2022-05-26 20:32:48 -0700

    update

--- swin_transformer/train.py
+++ swin_transformer/train.py
@@ -29,7 +29,7 @@ tensorflow.config.list_physical_devices('GPU')
 parser = argparse.ArgumentParser(description='SWIN')
 parser.add_argument('--batch_size', type=int, default=64,
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=10,
+parser.add_argument('--num_epoch', type=int, default=10,
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--decay_step', type=int, default=100,
                     help='number of step to for one decay (default: 100)')
